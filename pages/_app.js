8454665aa617dc4e590aebb243a766ca6b209521
SUPER VISUAL <114834716+supervisualuk@users.noreply.github.com>
2023-02-11 15:10:09 +0000

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,6 @@
-import { useTheme } from 'next-themes'
 import '../styles/global.css'
 
 export default function App({ Component, pageProps }) {
   return 
-     <ThemeProvider>
       <Component {...pageProps} />
-    </ThemeProvider>
 }
