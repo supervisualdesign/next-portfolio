d046f684f24441b72417442329e4ab522f5a0186
SUPER VISUAL <114834716+supervisualuk@users.noreply.github.com>
2023-02-11 15:15:31 +0000

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,4 @@
 import '../styles/global.css'
 
-export default function App({ Component, pageProps }) {
-  return 
-      <Component {...pageProps} />
-}
+ export default function App({ Component, pageProps }) {
+   return <Component {...pageProps} />
